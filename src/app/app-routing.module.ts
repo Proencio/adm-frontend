7992b02579456444d1ffefd7ea268818c51b7a72
johnproencio@hotmail.com <johnproencio@hotmail.com>
2022-11-17 16:14:44 -0300

    configurando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UsuariosComponent } from './components/usuarios/usuarios/usuarios.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'usuarios',
+    component: UsuariosComponent
   },
   // {
   //   path: 'produtos',
